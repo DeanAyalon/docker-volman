0bb73320658f6c1980d51a8af5c768084c6a589b
Dean Ayalon <deanayalon@me.com>
2024-06-28 12:03:15 +0300

    Fix script working directory, add gitignore

--- backup-docker-volumes.sh
+++ backup-docker-volumes.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 # Create a directory for the backups if it doesn't exist
-mkdir -p ${PWD}/archive
+backup_dir="$(dirname "$0")/archive"
+# PWD will give the user's execution directory, not this repository
+mkdir "$backup_dir"
+# -p is unnecessary
 
 # Get the list of docker volumes
 volumes=$(docker volume ls -q)
@@ -11,5 +14,5 @@ for source_volume in $volumes; do
   backup_date=$(date +"%Y%m%d_%H%M%S")
   backup_file="${source_volume}_${backup_date}.tar.gz"
   echo "Creating backup for volume: ${source_volume}, backup file: ${backup_file}"
-  docker run --tty --rm --interactive --volume ${source_volume}:/source --volume ${PWD}/archive:/backup alpine tar czvf /backup/${backup_file} -C /source .
+  docker run --tty --rm --interactive --volume ${source_volume}:/source --volume ${backup_dir}:/backup alpine tar czvf /backup/${backup_file} -C /source .
 done
