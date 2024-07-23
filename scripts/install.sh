522d0f56ec89e0944df0c2fe765c2ae4ed9f50e4
Dean Ayalon <dev@deanayalon.com>
2024-07-23 22:04:02 +0300

    Python installation file - implement install requirements

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 
+# Constants
+reqs=requirements.txt
+
 # Execution context
 cd "$(dirname "$0")/.."
 
 if [ -z "$1" ]; then
     # Install the packages specified in requirements.txt
-    echo unimplemented
+    echo Installing requirements...
+    pip3 install -r $reqs
 else
     # Install defined packages
     for package in "$@"; do
         echo Installing $package
         pip3 install $package || exit 1
-        pip3 freeze | grep -i $package >> requirements.txt
+        pip3 freeze | grep -i $package >> $reqs
     done
-fi
 
-echo 
-echo Requiremnets:
-cat requirements.txt
+    echo
+    echo Requiremnets:
+    cat $reqs
+fi 
