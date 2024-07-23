78210b4f45f7fa5848c02af8d7a22cb3584d7ed3
Dean Ayalon <dev@deanayalon.com>
2024-07-23 23:10:58 +0300

    Dynamically create Volman with existing volumes mounted

--- run.py
+++ run.py
@@ -1,3 +1,14 @@
+#!python3
+
 import docker
 client = docker.from_env()
-print(client.containers.create('ubuntu:22.04', 'tail -f /dev/null').start())
+
+# Define mounts by volume
+volumes = client.volumes.list()
+mounts = []
+for volume in volumes: mounts.append(volume.name + ':/volman/volumes/' + volume.name)
+
+# Start Volman
+volman = client.containers.run('ubuntu:22.04', ['tail', '-f', '/dev/null'], stderr = True, 
+                               detach = True, volumes = mounts, name = 'volman', hostname = 'volman',
+                               labels = { 'com.docker.compose.project': 'volman' })
