4af6200e2eb05a239c97567429f0131af0a7cc77
Dean Ayalon <dev@deanayalon.com>
2024-07-24 12:20:42 +0300

    Stop Volman if running

--- run.py
+++ run.py
@@ -12,9 +12,14 @@ volumes = client.volumes.list()
 mounts = []
 for volume in volumes: mounts.append(volume.name + ':/volman/volumes/' + volume.name)
 
-# Start Volman
+# Stop volman if running
 try: 
-    volman = cast(Container, client.containers.run('ubuntu:22.04', ['tail', '-f', '/dev/null'], stderr = True, 
+    volman = client.containers.get('volman')
+    volman.remove(force = True)
+except Exception as e: print(e)
+
+# Start Volman
+try: volman = cast(Container, client.containers.run('ubuntu:22.04', ['tail', '-f', '/dev/null'], stderr = True, 
                                                     detach = True, volumes = mounts, name = 'volman', hostname = 'volman',
                                                     labels = { 'com.docker.compose.project': 'volman' }))
 except Exception as e: print(e); exit()
