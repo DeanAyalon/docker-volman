874ea7d3bb18b0fd25364fc0fd77bf3431922714
Dean Ayalon <deanayalon@me.com>
2024-10-23 00:01:44 +0300

    Test Python binary - slow as heck??

---
+++ volman/pyinstaller.py
@@ -0,0 +1,13 @@
+import PyInstaller.__main__
+from pathlib import Path
+
+HERE = Path(__file__).parent.absolute()
+path_to_main = str(HERE / "__init__.py")
+
+def install():
+    PyInstaller.__main__.run([
+        path_to_main,
+        '--onefile',
+        '--windowed',
+        # other pyinstaller options... 
+    ])
